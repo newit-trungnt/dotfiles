00245b23139ce2773a5aa20cd6719e9b5479286b
newit.trungnt <trungnt@newit.co.jp>
2022-09-19 14:59:08 +0700

    chore: update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,5 +17,9 @@ packer.startup(function(use)
 	'nvim-lualine/lualine.nvim',
   	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
    }
+  use {
+    'nvim-telescope/telescope.nvim', tag= '0.1.0',
+  }
+  use "nvim-lua/plenary.nvim"
   -- Your plugins go here
 end)
