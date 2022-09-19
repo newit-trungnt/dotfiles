8de8c5d41aede4e932eff6fa85c2b02564b7b8cf
newit.trungnt <trungnt@newit.co.jp>
2022-09-19 15:35:35 +0700

    chore: update base + plugin

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,5 +1,5 @@
 vim.cmd("autocmd!")
-vim.cmd("colorscheme nordfox")
+vim.cmd("colorscheme dracula")
 
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,5 +22,6 @@ packer.startup(function(use)
   }
   use "nvim-lua/plenary.nvim"
   use { "nvim-telescope/telescope-file-browser.nvim" }
+  use { "dracula/vim", name = "dracula" }
   -- Your plugins go here
 end)
